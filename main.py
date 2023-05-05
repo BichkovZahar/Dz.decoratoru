ef81e0a7fe23caddb04f8c688ae478f9b5964b9c
Asus <Bichk_di78@student.itstep.org>
2023-05-05 20:52:02 +0300

    Lesson_4

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-def logger(func):
-    def wrapper(*args , **kwargs):
-        suma = func(*args , **kwargs)
-        print(f"Название функции: {func.__name__}  "
-              f"\nТемпература в Цельсіях: {suma}")
-        return suma * 9/5 + 32
+import time
+print("Загрузка...")
+def time_second(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        time.sleep(1.7)
+        result = func(*args, **kwargs)
+        finish = time.time()
+        print(f"Название функцыи: {func.__name__}  Время: {round(finish - start , 2)} секунд ")
+        return result
     return wrapper
-@logger
-def lesson_3(number):
-    return number
-print("Температура в Фаренгейтах:" , round(lesson_3(-59) , 2))
+@time_second
+def lesson_4(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5)+1):
+        if number % i == 0:
+            return False
+    return True
+print("Проверка на простое число:", lesson_4(56))
